f902a24896ce48a8e01665a75c1a93c849b26784
cemvarol <cem.varol@outlook.com>
2021-01-28 09:52:23 +0000

    Update iis-config.ps1

--- iis-config.ps1
+++ iis-config.ps1
@@ -1,2 +1,13 @@
 add-WindowsFeature Web-Server, Web-Mgmt-Tools
 Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
+
+
+netsh advfirewall set allprofiles state off
+
+$url = "https://github.com/cemvarol/AZ-Migration/blob/master/ChromeSetup.exe?raw=true"
+$output = "$env:USERPROFILE\downloads\ChromeSetup.exe"
+Invoke-WebRequest -Uri $url -OutFile $output
+
+& "$env:USERPROFILE\downloads\ChromeSetup.exe"
+
+
